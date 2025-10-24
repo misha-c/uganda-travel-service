eb285a1d64a9c2c689fd678d1a1e96a9712e8132
Misha <126611479+misha-c@users.noreply.github.com>
2025-10-24 14:19:05 -0700

    Update env config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isNetlify = process.env.NETLIFY === 'true';
+
 // https://astro.build/config
 export default defineConfig({ 
-    site: 'https://misha-c.github.io/uganda-travel-service/',
-    base: '/uganda-travel-service', 
-    output: "static",
+    site: 'https://ecotribeadventures.netlify.app/',
+    base: isNetlify ? '/' : '/uganda-travel-service',
+    output: 'static',
 });
